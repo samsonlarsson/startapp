eaba60ed45058e60d2b567491bcc4f9805589962
samsonpaul <samsonpaul4@gmail.com>
2017-01-17 16:59:15 +0200

    quick updates

--- startapp/extensions.py
+++ startapp/extensions.py
@@ -6,10 +6,10 @@ from flask_debugtoolbar import DebugToolbarExtension
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf_csrf import CsrfProtect
+from flask_wtf.csrf import CsrfProtect
 
 bcrypt = Bcrypt()
-csrf = CsrfProtect(app)
+csrf_protect = CsrfProtect()
 login_manager = LoginManager()
 db = SQLAlchemy()
 migrate = Migrate()
